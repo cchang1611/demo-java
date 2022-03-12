f48469d3f74a975e013d2aa4eb355a78e3f3f6b9
ddelacruz05 <david.delacruzs@outlook.com>
2022-03-12 16:34:15 -0500

    Proyecto inicial

---
+++ src/main/java/com/company/demo/controller/PrincipalController.java
@@ -0,0 +1,13 @@
+package com.company.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PrincipalController {
+
+    @GetMapping("/")
+    public String principal() {
+        return "index";
+    }
+}
