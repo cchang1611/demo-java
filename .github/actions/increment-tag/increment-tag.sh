8a74b604a375d8445606a1bb909aaf9ef7feb7ab
cchang1611 <97133916+cchang1611@users.noreply.github.com>
2023-07-06 18:46:59 -0500

    Update increment-tag.sh

--- .github/actions/increment-tag/increment-tag.sh
+++ .github/actions/increment-tag/increment-tag.sh
@@ -5,35 +5,35 @@ TAG=$1
 TAG_PREFIX=${TAG%-snapshot}  # Elimina la palabra "snapshot" al final del tag
 TAG_SUFFIX=${TAG#"$TAG_PREFIX"}  # Obtiene la parte del tag después del prefijo
 
-if ($TAG -match '^(\d+)\.(\d+)\.(\d+)$') {
+if [$TAG -match '^(\d+)\.(\d+)\.(\d+)$']; then
     $major = [int]$Matches[1]
     $minor = [int]$Matches[2]
     $patch = [int]$Matches[3]
 
     # Incrementar el patch hasta llegar a 9
-    if ($patch -lt 9) {
+    if [$patch -lt 9]; then
         $patch++
-    } else {
+    else
         # Incrementar el minor y reiniciar el patch a 0
-        if ($minor -lt 9) {
+        if [$minor -lt 9]; then
             $minor++
             $patch = 0
-        } else {
+        else 
             # Incrementar el major, reiniciar el minor y el patch a 0
-            if ($major -lt 9) {
+            if [$major -lt 9]; then
                 $major++
                 $minor = 0
                 $patch = 0
-            }
-        }
-    }
+            fi
+        fi
+    fi
 
     $newTag = "$major.$minor.$patch"
     $newTagWithSnapshot = $newTag + "-snapshot"
     Write-Host "Nuevo tag: $newTagWithSnapshot"
     
-} else {
+else 
     Write-Host "El tag no tiene el formato esperado"
-}
+fi
 
 echo "Nuevo Tag es: $newTag"
